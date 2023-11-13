7c086d64640b2ad732ace2a87e5897170424382e
MariaZinevych <123274407+MariaZinevych@users.noreply.github.com>
2023-11-13 22:36:21 +0100

    add env file

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
-const app = require("./app");
 const mongoose = require("mongoose");
+const app = require("./app");
 
-// const { DB_HOST, PORT = 3000 } = process.env;
-const DB_HOST =
-  "mongodb+srv://Mariia:AftpTkxwlqbOy4xB@cluster0.zrjkesn.mongodb.net/db-contacts?retryWrites=true&w=majority";
+const { DB_HOST, PORT = 3000 } = process.env;
+
 mongoose.set("strictQuery", true);
 
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    app.listen(3000);
+    app.listen(PORT);
     console.log("Database connection successful");
   })
   .catch((error) => {
